92f94774375603f6674d0c60179e77334fd509fa
openfe222 <openfe@163.com>
2021-01-15 21:25:40 +0800

    add: 自动压缩文件夹

--- webpackConf/webpack.prod.js
+++ webpackConf/webpack.prod.js
@@ -1,14 +1,26 @@
 const merge = require('webpack-merge')
 const webpackBaseConf = require('./webpack.base')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 module.exports = merge(webpackBaseConf, {
 	mode: 'production',
 	output: {
 		filename: '[name].js',
 		publicPath: '../'
 	},
-	plugins: [
+	plugins: [	
+		new FileManagerPlugin({
+            events:{
+                onEnd: {
+                    　　 delete: [
+                       　　  './dist/innermanage.zip',
+                   　　  ],
+                    　　 archive: [
+                      　　   {source: './dist', destination: './dist/innermanage.zip'},
+                     　　]
+                　　 }
+            }　　   
+        }),
 		new OptimizeCSSPlugin({
 			assetNameRegExp: /\.css$/g,
 			cssProcessor: require('cssnano'),
